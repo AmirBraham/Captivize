6c50943e7c88e9aaeed1cb8b1fafe5cdbb335eb8
amirbraham <amirbrahamm@gmail.com>
2024-05-28 14:50:28 +0200

    fixed bug with auth

--- app/projects/[project_id]/layout.js
+++ app/projects/[project_id]/layout.js
@@ -8,38 +8,41 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 export default async function LayoutProjectPrivate({ children, params }) {
     const supabase = createServerComponentClient({ cookies });
     const project_id = params["project_id"]
-    if(!project_id) {
+    if (!project_id) {
         console.log("missing project id ! ")
         redirect(config.auth.loginUrl);
-        
-    }
-    const { user, userError } = await supabase.auth.getUser()
 
-    if (!user) {
+    }
+    const {
+        data: { session },
+    } = await supabase.auth.getSession();
+
+    if (!session) {
         // Redirect to login if user is not authenticated
+        console.log("user not authenticated")
         redirect(config.auth.loginUrl);
     }
-    if (userError) {
-        console.error('Error fetching user:', userError);
-        redirect(config.auth.loginUrl);
-    }
-
-    const userId = user.id;
-
+    const userId = session.user.id;
     const { data, projectError } = await supabase
         .from('projects')
         .select('*')
         .eq('id', project_id)
         .single();
 
+
     if (projectError) {
         console.error('Error fetching project:', projectError);
+        redirect(config.auth.loginUrl);
+    }
+    if (!data) {
+        console.log("project id does not exist")
         redirect(config.auth.loginUrl);
     }
 
     if (data && data.user_id === userId) {
         return <>{children}</>
     } else {
+        console.log(data)
         redirect(config.auth.loginUrl);
     }
 
